e3ce2afa8e49d25c8637dfbaa6a71b92374675f6
VaishnaviHonale <137199492+VaishnaviHonale@users.noreply.github.com>
2023-08-25 18:54:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -34,8 +34,23 @@ const lookup = {
 function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
-
-  return; //return decodedArr
+	 var regEx = /[A-Z]/;
+  str = str.split("");
+  for (var x in str) {
+    if (regEx.test(str[x])) {
+      // A more general approach
+      // possible because of modular arithmetic
+      // and cyclic nature of rot13 transform
+      rotCharArray.push(((str[x].charCodeAt() - 65 + 13) % 26) + 65);
+    } else {
+      rotCharArray.push(str[x].charCodeAt());
+    }
+  }
+  str = String.fromCharCode.apply(String, rotCharArray);
+  return str;
+	
+   
+  r //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
